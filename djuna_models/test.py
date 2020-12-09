4fd27573888b6596d21f7d9c54a98f9789e8bc93
Djuna <djunavm@gmail.com>
2020-12-08 22:46:19 -0500

    pairwise dist scatter

---
+++ djuna_models/test.py
@@ -0,0 +1,8 @@
+#%%
+
+direct_leftout = '/Users/work/Documents/GitHub/mlgenomics/data_preprocessing/Wang_2018_all_2D_3D_processed_new.npz'
+
+content_leftout = np.load(direct_leftout)['arr_0']
+# %%
+import numpy as np
+# %%
